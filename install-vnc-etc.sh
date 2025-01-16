c2b77522e7a9e5e620e7d56cd5233581c4712920
DorjeDevel <dole.fifer.0w@icloud.com>
2025-01-16 20:58:49 +0100

    Updated pCloud Section with correct URL and error handling

--- install-vnc-etc.sh
+++ install-vnc-etc.sh
@@ -214,6 +214,12 @@ timeshift --create --comments "NordVPN installed" --tags D
 ##
 ##   Install pCloud
 ##
+##   Original script from:
+##   https://surajdeshpande.wordpress.com/2021/01/18/upgrade-pcloud-version-on-ubuntu-using-shell-script/
+##   
+##   Updated and extended by DorjeDevel
+##   Version: 2025-01-16 
+##   
 ## ########################################################################################################
 
 # Determine the original user and their home directory
@@ -226,36 +232,111 @@ if [ "$(uname -m)" != "x86_64" ]; then
   exit 1
 fi
 
-# Download pCloud AppImage
-echo "Downloading pCloud AppImage..."
-wget -O "$USER_HOME/pcloud" https://download.pcloud.com/latest/x86_64/pcloud
+# Change directory to downloads folder
+echo 'Jumping to Downloads...'
+cd /home/$EXEC_USER/Downloads
+pwd 
+echo
 
-# Make the AppImage executable
-chmod +x "$USER_HOME/pcloud"
+# Delete existing pcloud file
+echo "Removing existing pcloud file in Downloads..."
+rm -f pcloud
+echo
 
-# Start pCloud
-echo "Starting pCloud..."
-sudo -u "$EXEC_USER" "$USER_HOME/pcloud" &
+# Download new Pcloud file
+# The web browser URL is
+# wget https://www.pcloud.com/how-to-install-pcloud-drive-linux.html?download=electron-64
+# but wget or curl wont find the right file but instead would download the web page.
+# You have to check with F12 in browser (choose network > Media and click on the GET line "p-lux1.cloud.com") to see the correct URL in the file-headers area on the right side of the F12-dev-window.
 
-# Add pCloud to startup applications
-echo "Adding pCloud to startup applications..."
-mkdir -p "$USER_HOME/.config/autostart"
-cat > "$USER_HOME/.config/autostart/pcloud.desktop" <<EOL
-[Desktop Entry]
-Type=Application
-Exec=$USER_HOME/pcloud
-Hidden=false
-NoDisplay=false
-X-GNOME-Autostart-enabled=true
-Name=pCloud
-Comment=pCloud Drive
-EOL
+# URL of the file
+url="https://p-lux1.pcloud.com/cBZOomEB3Zd6YO7b7ZZZGriWXkZ2ZZa55ZkZ9U8xVZz8ZTzZSYZpRZz4Z1pZnLZdQZpQZEQZGQZyzZU8ZYQZxqNX5ZbYH5fYNIg0Q9UsLkb78hpy1gpDzk/pcloud"
 
-# Set correct ownership for the user
-chown "$EXEC_USER:$EXEC_USER" "$USER_HOME/pcloud"
-chown -R "$EXEC_USER:$EXEC_USER" "$USER_HOME/.config/autostart"
+# Target file name
+output_file="pcloud"
 
-echo "Installation complete. pCloud has been added to startup and should now be running."
+# Minimum size in bytes (50 MB = 50 * 1024 * 1024)
+min_size=$((50 * 1024 * 1024))
+
+# Download the file
+echo "Downloading file..."
+wget "$url" -O "$output_file"
+
+# Check if the download was successful
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to download the file."
+  exit 1
+fi
+
+# Get the size of the downloaded file
+file_size=$(stat -c%s "$output_file")
+
+# Display the file size
+echo "File size: $file_size bytes"
+
+# Check the size
+if [ "$file_size" -ge "$min_size" ]; then
+  # File is large enough. Proceeding with the script.
+
+  # Allow executing file as a program
+  echo 'Making file executable...'
+  chmod +x pcloud
+ 
+  # Kill existing pcloud processes
+  echo 'Killing existing pcloud processes...'
+  pgrep -u $EXEC_USER -f pcloud | xargs -r kill -9
+
+  # Copy updated version to bin folder
+  echo 'Copying updated version to bin folder...'
+  sudo cp pcloud /usr/bin/
+  echo 
+  echo 'Starting pCloud...'
+  nohup pcloud > /dev/null 2>&1 &
+
+  # add pcloud to autostart
+
+  # name of autostart app
+  autostart_file="$HOME/.config/autostart/pcloud.desktop"
+
+  # make sure autostart directory exists
+  mkdir -p "$HOME/.config/autostart"
+
+  # Inhalt der .desktop-Datei erstellen
+  autostart_file="$HOME/.config/autostart/pcloud.desktop"
+  mkdir -p "$HOME/.config/autostart"  
+  
+  echo "[Desktop Entry]" > "$autostart_file"
+  echo "Type=Application" >> "$autostart_file"
+  echo "Exec=$HOME/pcloud" >> "$autostart_file"
+  echo "Hidden=false" >> "$autostart_file"
+  echo "NoDisplay=false" >> "$autostart_file"
+  echo "X-GNOME-Autostart-enabled=true" >> "$autostart_file"
+  echo "Name=pCloud" >> "$autostart_file"
+  echo "Comment=Start pCloud at login" >> "$autostart_file"
+    
+  echo "pCloud has been added to autostart."
+
+  # Set correct ownership for the user
+  chown "$EXEC_USER:$EXEC_USER" "$USER_HOME/pcloud"
+  chown -R "$EXEC_USER:$EXEC_USER" "$USER_HOME/.config/autostart"
+
+  echo "Installation complete. pCloud has been added to startup and should now be running."
+
+else
+  # File size is too small to be the pcloud app
+  echo "File is too small. Deleting file..."
+  echo "Please check the download URL in this script as it seems to have changed. There is a small how to in the comment."
+  rm "$output_file"
+fi
+
+
+# Wait for User to return
+echo "Press RETURN key to continue the script..."
+read dummy
+
+
+
+
 
 
 # --------------------------------------------------------------------------------------------------------
