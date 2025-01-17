3a061299b2ca64aa5768550dde1885fbf52907e7
DorjeDevel <dole.fifer.0w@icloud.com>
2025-01-18 00:33:02 +0100

    Update main_installer.sh

--- seperate scripts/main_installer.sh
+++ seperate scripts/main_installer.sh
@@ -1,7 +1,32 @@
-
 #!/bin/bash
 
+clear
+
+echo
+echo "------------------------------------------------------------------------------"
+echo 
 echo "Welcome to the App Installer!"
+echo 
+echo "You can now install x11VNC, Enpass, NordVPN, pCloud & Mediathekview."
+echo 
+echo "After installing NordVPN, reboot and restart this script to continue."
+echo
+echo "------------------------------------------------------------------------------"
+echo
+
+# Ask the user if they want to take a snapshot
+read -p "Do you want to take a snapshot before proceeding? (y/n): " snapshot_choice
+if [[ $snapshot_choice == "y" || $snapshot_choice == "Y" ]]; then
+    echo "Creating a snapshot..."
+    # Replace the following line with your snapshot creation command
+    echo "Snapshot created successfully!"
+else
+    echo "On your own risk."
+fi
+
+echo
+echo "------------------------------------------------------------------------------"
+echo
 apps=("VNC" "Enpass" "NordVPN" "pCloud" "Mediathekview")
 
 for app in "${apps[@]}"; do
@@ -17,4 +42,10 @@ for app in "${apps[@]}"; do
     fi
 done
 
+echo "------------------------------------------------------------------------------"
 echo "Installation process complete!"
+echo "Please check the output above!"
+echo "Have Fun!"
+echo
+
+
